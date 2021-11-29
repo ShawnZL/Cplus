670f323476af5bcb9ceabe580f3df24900bd9960
Shawn <1140068696@qq.com>
2021-11-29 15:36:24 +0800

    20211129

---
+++ chp4/4.23/cmake-build-debug/CMakeFiles/4_23.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_23.exe"
+  "4_23.exe.manifest"
+  "4_23.pdb"
+  "CMakeFiles/4_23.dir/mixtypes.cpp.obj"
+  "lib4_23.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
