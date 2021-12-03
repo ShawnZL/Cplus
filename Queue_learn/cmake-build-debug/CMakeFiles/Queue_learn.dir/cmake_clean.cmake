cbed388bec70527ca020af1d53c94ad9b573a9ba
Shawn <1140068696@qq.com>
2021-12-03 11:52:41 +0800

    20211203

---
+++ Queue_learn/cmake-build-debug/CMakeFiles/Queue_learn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Queue_learn.dir/main.cpp.obj"
+  "Queue_learn.exe"
+  "Queue_learn.exe.manifest"
+  "Queue_learn.pdb"
+  "libQueue_learn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Queue_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chp4/4.24/cmake-build-debug/CMakeFiles/4_24.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_24.exe"
+  "4_24.exe.manifest"
+  "4_24.pdb"
+  "CMakeFiles/4_24.dir/choices.cpp.obj"
+  "lib4_24.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
