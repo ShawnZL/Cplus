bc15a60e85088ff95dcdca769516d246605bc582
Shawn Zhao <shawnzhao@ShawndeMacBook-Pro.local>
2022-04-04 22:40:50 +0800

    finish chp14

--- chp14/14.22/main.cpp
+++ chp14/14.22/main.cpp
@@ -36,6 +36,15 @@ int main() {
     counts();
     HasFriend<int> hfi1(10);
     cout << "After hfi1 declared: ";
-    counts()
+    counts();
+    HasFriend<int> hfi2(20);
+    cout << "After hfi2 declard: ";
+    counts();
+    HasFriend<double> hfdb(10.5);
+    cout << "After hfdb declared: ";
+    counts();
+    reports(hfi1);
+    reports(hfi2);
+    reports(hfdb);
     return 0;
 }
